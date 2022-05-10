f14d3c6da28883a27f2b597769b441b7b55f1795
Armand Marechal <sypenfu@gmail.com>
2022-05-10 23:04:55 +0200

    Add Subnets to Vpc class

--- src/Vpc.js
+++ src/Vpc.js
@@ -2,6 +2,7 @@
 
 const AWS = require('aws-sdk');
 const ec2 = new AWS.EC2({ region: 'eu-west-3' });
+const Subnet = require('./Subnet.js');
 
 const VpcNotFoundException = require('./exceptions/VpcNotFoundException.js');
 
@@ -9,22 +10,23 @@ module.exports = class Vpc {
 	//region private attributes
 	#id;
 	#ipRange;
+	#subnets;
 	//endregion private attributes
 
 	//region public methods
 
-	/**
-	 * @brief Constructor of the class Vpc, which instantiates a Vpc.
-	 * @param distinct : boolean a distinct constraint
-	 * @returns {void}
-	 * @exception NonExistingVpcException is thrown if the vpc doesn't exist.
-	 * @link https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/EC2.html#describeVpcs-property
-	 */
-	constructor(id, ipRange) {
+	constructor(id, ipRange, subnets) {
 		this.#id = id;
 		this.#ipRange = ipRange;
+		this.#subnets = subnets;
 	}
 
+	/**
+	 * @brief Fetches the VPC with the given id from the AWS EC2 SDK and returns a Vpc object
+	 * @returns {void}
+	 * @exception VpcNotFoundException is thrown if the vpc doesn't exist.
+	 * @link https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/EC2.html#describeVpcs-property
+	 */
 	static find(id) {
 		return new Promise((resolve, reject) => {
 			ec2.describeVpcs({ VpcIds: [id] }, (err, data) => {
@@ -33,15 +35,28 @@ module.exports = class Vpc {
 						reject(new VpcNotFoundException(id));
 					} else {
 						reject(err);
-					}	
+					}
 				} else {
-					const vpc = new Vpc(
-						data.Vpcs[0].VpcId,
-						data.Vpcs[0].CidrBlock
-						// TODO: Implement subnets
-					);
+					const vpc = new Vpc(data.Vpcs[0].VpcId, data.Vpcs[0].CidrBlock, null);
 					resolve(vpc);
 				}
+			});
+		}).then((vpc) => {
+			return new Promise((resolve, reject) => {
+				ec2.describeSubnets(
+					{ Filters: [{ Name: 'vpc-id', Values: [vpc.id] }] },
+					(err, data) => {
+						if (err) {
+							reject(err);
+						} else {
+							const subnets = data.Subnets.map((subnet) => {
+								return new Subnet(subnet.SubnetId);
+							});
+							vpc.subnets = subnets;
+							resolve(vpc);
+						}
+					}
+				);
 			});
 		});
 	}
@@ -52,5 +67,13 @@ module.exports = class Vpc {
 
 	get ipRange() {
 		return this.#ipRange;
+	}
+
+	get subnets() {
+		return this.#subnets;
+	}
+
+	set subnets(subnets) {
+		this.#subnets = subnets;
 	}
 };
