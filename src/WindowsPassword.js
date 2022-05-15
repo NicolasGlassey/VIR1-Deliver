fb5ff66a9427b394a1a64e3fd72564c3dfadf596
Yannick Clifford <yannickbaudrazdev@gmail.com>
2022-05-15 20:38:40 +0200

    Handle Windows password exceptions

--- src/WindowsPassword.js
+++ src/WindowsPassword.js
@@ -2,6 +2,8 @@
 
 const AWS = require('aws-sdk');
 const ec2 = new AWS.EC2({ region: 'eu-west-3' });
+const InstanceNotFoundException = require("./exceptions/InstanceNotFoundException.js");
+const UnavailableInstancePasswordException = require("./exceptions/UnavailableInstancePasswordException.js");
 
 module.exports = class WindowsPassword {
     #instanceId
@@ -21,9 +23,18 @@ module.exports = class WindowsPassword {
      * @see https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/EC2.html#getPasswordData-property
      */
     static async find(instanceId) {
-        const result = await ec2.getPasswordData({
-            InstanceId: instanceId,
-        }).promise()
+        const handleError = err => {
+            if (err.code.includes('InvalidInstanceID'))
+                throw new InstanceNotFoundException(err.message);
+            throw err;
+        };
+
+        const result = await ec2.getPasswordData({ InstanceId: instanceId, })
+                                .promise()
+                                .catch(handleError);
+
+        if (!result.PasswordData)
+            throw new UnavailableInstancePasswordException(`The password of instance ${instanceId} is not available.`);
 
         return new WindowsPassword(result.InstanceId, result.PasswordData, result.Timestamp);
     }
