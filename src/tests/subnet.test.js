51fb264e3b1663d14e810e8c578b995e0694a827
Armand Marechal <sypenfu@gmail.com>
2022-05-16 18:03:12 +0200

    Add subnet unit test for all method on non existing vpc

--- src/tests/subnet.test.js
+++ src/tests/subnet.test.js
@@ -1,16 +1,29 @@
 const Subnet = require('../Subnet.js');
-const VpcNotFoundException = require('../exceptions/VpcNotFoundException.js');
 
-test('find_ExistingVpc_Success', async () => {
-	// Given
-	const givenVpcId = 'vpc-08584e8bf7e83d040';
-	const expectedSubnetIds = ['subnet-00ebe6783616bc17c'];
+describe('Subnet', () => {
+	test('all_ExistingVpc_Success', async () => {
+		// Given
+		const givenVpcId = 'vpc-08584e8bf7e83d040';
+		const expectedSubnetIds = ['subnet-00ebe6783616bc17c'];
 
-	// When
-	const subnets = await Subnet.all(givenVpcId);
+		// When
+		const subnets = await Subnet.all(givenVpcId);
 
-	// Then
-	for (let i = 0; i < expectedSubnetIds.length; i++) {
-		expect(subnets[i].id).toEqual(expectedSubnetIds[i]);
-	}
+		// Then
+		for (let i = 0; i < expectedSubnetIds.length; i++) {
+			expect(subnets[i].id).toEqual(expectedSubnetIds[i]);
+		}
+	});
+
+	test('all_NonExistingVpc_ThrowException', async () => {
+		// Given
+		const wrongVpcId = 'vpc-id-which-does-not-exist';
+		const expectedSubnetIds = [];
+
+		// When
+		const subnets = await Subnet.all(wrongVpcId);
+
+		// Then
+		expect(subnets).toEqual(expectedSubnetIds);
+	});
 });
