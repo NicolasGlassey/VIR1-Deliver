3e6179d5dcc65975005bf12d5522b1577ed0be56
Yannick Baudraz <yannick.baudraz@cpnv.ch>
2022-05-24 10:16:41 +0200

    Change name to helper

--- src/tests/security-group.test.js
+++ src/tests/security-group.test.js
@@ -1,26 +1,26 @@
-const SecurityGroup = require('../SecurityGroup.js');
+const SecurityGroup = require('../SecurityGroupHelper.js');
 
-describe('SecurityGroup', () => {
-    test('all_BasicCase_Success', async () => {
+describe('SecurityGroupHelper', () => {
+    test('describe_BasicCase_Success', async () => {
         // Given
         const expectedId = 'sg-004f6547219fdef6c';
         const expectedDescription = 'launch-wizard-2 created 2020-12-04T08:21:57.536+01:00';
         const vpcId = 'vpc-08584e8bf7e83d040';
 
         // When
-        const securityGroups = await SecurityGroup.all(vpcId);
+        const securityGroups = await SecurityGroup.describe(vpcId);
 
         // Then
         expect(securityGroups[1].id).toBe(expectedId);
         expect(securityGroups[1].description).toBe(expectedDescription);
     });
 
-    test('all_VpcWithoutSecurityGroups_EmptyArray', async () => {
+    test('describe_VpcWithoutSecurityGroups_EmptyArray', async () => {
         // Given
         const vpcId = 'vpc-11111111111111111';
 
         // When
-        const securityGroups = await SecurityGroup.all(vpcId);
+        const securityGroups = await SecurityGroup.describe(vpcId);
 
         // Then
         expect(securityGroups).toEqual([]);
