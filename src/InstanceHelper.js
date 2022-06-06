478ad1624027165ee0e7bce1ef1057190ae2eeda
Armand Marechal <sypenfu@gmail.com>
2022-06-06 13:06:01 +0200

    Refactor Subnet with instance

--- src/InstanceHelper.js
+++ src/InstanceHelper.js
@@ -2,27 +2,30 @@
 
 const AWS = require('aws-sdk');
 const ec2 = new AWS.EC2({ region: 'eu-west-3' });
-
 const Logger = require('./FileLogger')
 
 const InstanceNotFoundException = require('./exceptions/instance/InstanceNotFoundException.js');
 
 module.exports = class InstanceHelper {
+    //region public methods
+
     /**
      * @brief Check if the given name exists from the AWS EC2 SDK
      * @param name {Promise<string>} name of an Instance
      * @returns {boolean} true if the Instance exists, false otherwise
      * @see https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/EC2.html#describeInstances-property
      */
-    static async exists(name) {
+    async exists(name) {
         // function always return empty array even if instance does not exist
-        const handleError = err => {
+        const handleError = (err) => {
             Logger.error(err.message);
             throw err;
         };
 
         const result = await ec2
-            .describeInstances({ Filters: [{ Name: "tag:Name", Values: [name] }] })
+            .describeInstances({
+                Filters: [{ Name: "tag:Name", Values: [name] }],
+            })
             .promise()
             .catch(handleError);
 
@@ -36,23 +39,27 @@ module.exports = class InstanceHelper {
      * @exception InstanceNotFound is thrown if the there is no instance with that name
      * @see https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/EC2.html#describeInstances-property
      */
-    static async describe(name) {
-        const handleError = err => {
+    async describe(name) {
+        const handleError = (err) => {
             Logger.error(err.message);
             throw err;
         };
 
         const result = await ec2
-            .describeInstances({ Filters: [{ Name: "tag:Name", Values: [name] }] })
+            .describeInstances({
+                Filters: [{ Name: "tag:Name", Values: [name] }],
+            })
             .promise()
             .catch(handleError);
 
         const reservations = result.Reservations;
         if (reservations.length === 0) {
-            throw new InstanceNotFoundException;
+            throw new InstanceNotFoundException();
         }
         Logger.info(`Describe instance ${name}`);
 
         return reservations[0].Instances[0];
     }
+
+    //endregion public methods
 };
