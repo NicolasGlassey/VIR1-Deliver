2d917f5c5aff6ebb33de256b0edd48cf82d955ef
Yannick Clifford <yannickbaudrazdev@gmail.com>
2022-06-07 10:19:21 +0200

    Make WindowsPasswordHelper.describe not static

--- src/tests/windows-password.test.js
+++ src/tests/windows-password.test.js
@@ -1,15 +1,22 @@
-const WindowsPassword = require('../WindowsPasswordHelper.js');
+const WindowsPasswordHelper = require('../WindowsPasswordHelper.js');
 const InstanceNotFoundException = require('../exceptions/instance/InstanceNotFoundException.js');
 const UnavailableInstancePasswordException = require("../exceptions/instance/UnavailableInstancePasswordException.js");
 
 describe('WindowsPassword', () => {
+    /** @type {WindowsPasswordHelper} */
+    let windowsPassword;
+
+    beforeAll(async () => {
+        windowsPassword = new WindowsPasswordHelper();
+    });
+
     test('describe_ExistingInstanceName_Success', async () => {
         // Given
         const instanceName = 'WINDOWS_INSTANCE';
         const expectedInstanceId = 'i-0e7dcbe8cf352ad91';
 
         // When
-        const result = await WindowsPassword.describe(instanceName);
+        const result = await windowsPassword.describe(instanceName);
 
         // Then
         expect(result.InstanceId).toEqual(expectedInstanceId);
@@ -22,7 +29,7 @@ describe('WindowsPassword', () => {
         const instanceName = 'WINDOWS_INSTANCE_NON_EXISTING';
 
         // When
-        await expect(WindowsPassword.describe(instanceName)).rejects.toThrow(InstanceNotFoundException);
+        await expect(windowsPassword.describe(instanceName)).rejects.toThrow(InstanceNotFoundException);
 
         // Then
         // Exception is thrown
@@ -30,10 +37,10 @@ describe('WindowsPassword', () => {
 
     test('describe_LinuxInstanceName_ThrowException', async () => {
         // Given
-        const instanceId = 'debian';
+        const instanceName = 'debian';
 
         // When
-        await expect(WindowsPassword.describe(instanceId)).rejects.toThrow(UnavailableInstancePasswordException);
+        await expect(windowsPassword.describe(instanceName)).rejects.toThrow(UnavailableInstancePasswordException);
 
         // Then
         // Exception is thrown
