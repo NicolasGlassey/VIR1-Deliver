2f6591e407a55a923c49d934024ae4f248ed3ef5
Armand Marechal <sypenfu@gmail.com>
2022-06-07 09:29:12 +0200

    Update Function header

--- src/KeyPairHelper.js
+++ src/KeyPairHelper.js
@@ -1,24 +1,24 @@
-'use strict';
+"use strict";
 
-const AWS = require('aws-sdk');
-const ec2 = new AWS.EC2({ region: 'eu-west-3' });
-const Logger = require('./FileLogger')
+const AWS = require("aws-sdk");
+const ec2 = new AWS.EC2({ region: "eu-west-3" });
+const Logger = require("./FileLogger");
 
-const KeyPairNotFoundException = require('./exceptions/key_pair/KeyPairNotFoundException.js');
-const KEY_PAIR_NOT_FOUND = 'InvalidKeyPair.NotFound';
+const KeyPairNotFoundException = require("./exceptions/key_pair/KeyPairNotFoundException.js");
+const KEY_PAIR_NOT_FOUND = "InvalidKeyPair.NotFound";
 
 module.exports = class KeyPairHelper {
     /**
      * @brief Check if the given name exists from the AWS EC2 SDK
      * @param name {string} name of a KeyPair
-     * @returns {boolean} true if the KeyPair exists, false otherwise
+     * @returns {Promise<boolean>} true if the KeyPair exists, false otherwise
      * @see https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/EC2.html#describeVpcs-property
      */
-     async exists(name) {
+    async exists(name) {
         let params = {};
-        params['KeyNames'] = [name];
+        params["KeyNames"] = [name];
 
-        const handleError = err => {
+        const handleError = (err) => {
             Logger.error(err.message);
             if (err.code === KEY_PAIR_NOT_FOUND) {
                 return false;
@@ -26,9 +26,10 @@ module.exports = class KeyPairHelper {
             throw err;
         };
 
-        const result = await ec2.describeKeyPairs(params)
-                              .promise()
-                              .catch(handleError);
+        const result = await ec2
+            .describeKeyPairs(params)
+            .promise()
+            .catch(handleError);
 
         Logger.info(`Describe Keypair ${name} to check if it exists`);
         if (result) {
@@ -40,15 +41,16 @@ module.exports = class KeyPairHelper {
 
     /**
      * @brief Fetch a keypair from a name
-     * @returns {Promise}
+     * @param name {string} name of a KeyPair
+     * @returns {Promise<EC2.KeyPair>} KeyPair with the given name
      * @exception KeyPairNotFoundException is thrown if the there is no keypair with that name
      * @see https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/EC2.html#describeKeyPairs-property
      */
     async describe(name) {
         let params = {};
-        params['KeyNames'] = [name];
+        params["KeyNames"] = [name];
 
-        const handleError = err => {
+        const handleError = (err) => {
             Logger.error(err.message);
             if (err.code === KEY_PAIR_NOT_FOUND) {
                 throw new KeyPairNotFoundException(err.message);
@@ -56,12 +58,13 @@ module.exports = class KeyPairHelper {
             throw err;
         };
 
-        const keys = await ec2.describeKeyPairs(params)
-                              .promise()
-                              .catch(handleError);
+        const keys = await ec2
+            .describeKeyPairs(params)
+            .promise()
+            .catch(handleError);
         const key = keys.KeyPairs[0];
 
         Logger.info(`Describe Keypair ${key.KeyName}`);
         return key;
     }
-}
+};
