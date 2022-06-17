c5b250c6c697b1cb524770f22a915e9a42aa9451
Yannick Clifford <yannickbaudrazdev@gmail.com>
2022-06-17 21:37:17 +0200

    Write credentials in files

--- src/Credentials.js
+++ src/Credentials.js
@@ -3,25 +3,54 @@
 const KeyPairHelper = require("./KeyPairHelper");
 const fs = require("fs");
 const path = require("path");
+const WindowsPasswordHelper = require("./WindowsPasswordHelper");
 
 module.exports = class Credentials {
+    //region private fields
+
+    #outputDir = 'output';
+
+    //endregion
+
     //region public methods
 
     /**
-     * @brief Describe different credentials of the instance
+     * @brief Describe all Linux ssh keys
      * @returns {Promise<void>} Write the credentials in separate files
      */
     async describeLinuxSshKeys() {
         const keyPairs = await new KeyPairHelper().describe();
 
-        const outputDir = 'output';
-        if (!fs.existsSync(outputDir))
-            fs.mkdirSync(outputDir);
+        this.#createOutputDirIfNotExists();
 
         keyPairs.forEach(keyPair => {
             if (keyPair.PublicKey)
-                fs.writeFileSync(path.join(outputDir, keyPair.KeyPairId), keyPair.PublicKey);
+                fs.writeFileSync(path.join(this.#outputDir, keyPair.KeyPairId), keyPair.PublicKey);
         })
+    }
+
+    /**
+     * @brief Describe all Windows passwords
+     * @returns {Promise<void>} Write the credentials in separate files
+     */
+    async describeWindowsPasswords() {
+        const passwords = await new WindowsPasswordHelper().describe();
+
+        this.#createOutputDirIfNotExists();
+
+        passwords.forEach(password => {
+            if (password.PasswordData)
+                fs.writeFileSync(path.join(this.#outputDir, password.InstanceId), password.PasswordData);
+        })
+    }
+
+    //endregion
+
+    //region private methods
+
+    #createOutputDirIfNotExists() {
+        if (!fs.existsSync(this.#outputDir))
+            fs.mkdirSync(this.#outputDir);
     }
 
     //endregion
